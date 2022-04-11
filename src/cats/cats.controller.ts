ec343ecaf48d6f29e7649ddfcebb7a36424516ff
kntowd <kenta2231989@gmail.com>
2022-04-11 19:38:36 +0900

    DIをした

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,15 +1,19 @@
 import { Controller, Get, Post, Body } from '@nestjs/common';
+import { CatsService } from './cats.service';
 import { CreateCatDto } from './dto/create-cat-dto';
+import { Cat } from './interfaces/cat.interface';
 
 @Controller('cats')
 export class CatsController {
+  constructor(private catsService: CatsService) {}
+
   @Get()
-  findAll(): string {
-    return 'This action returns all cats';
+  async findAll(): Promise<Cat[]> {
+    return this.catsService.findAll();
   }
 
   @Post()
   async create(@Body() CreateCatDto: CreateCatDto) {
-    return 'This action adds a new cat';
+    this.catsService.create(CreateCatDto);
   }
 }
