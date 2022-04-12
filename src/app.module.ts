efdb711c7cd7fd0b442fc03461f889cdca940782
kntowd <kenta2231989@gmail.com>
2022-04-12 10:06:20 +0900

    middlewareを追加

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,14 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { CatsModule } from './cats/cats.module';
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
 
 @Module({
   imports: [CatsModule],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  // middleware設定用関数
+  configure(consumer: MiddlewareConsumer) {
+    // forRoutesは必須
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
