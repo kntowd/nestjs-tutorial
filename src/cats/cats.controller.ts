926faeee5f0da95850652ea4a7399c4956e31f88
kntowd <kenta2231989@gmail.com>
2022-04-12 11:04:39 +0900

    例外処理を追加

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
 import { CatsService } from './cats.service';
 import { CreateCatDto } from './dto/create-cat-dto';
 import { Cat } from './interfaces/cat.interface';
@@ -15,5 +22,11 @@ export class CatsController {
   @Post()
   async create(@Body() CreateCatDto: CreateCatDto) {
     this.catsService.create(CreateCatDto);
+  }
+
+  @Get('error')
+  async testError(): Promise<void> {
+    //　実際のレスポンス {"statusCode":403,"message":"Forbidden"}
+    throw new HttpException('Forbidden', HttpStatus.FORBIDDEN);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { cors } from './common/middleware/cors.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // 全てのルーティングに反映させるミドルウェア
   app.use(cors);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
